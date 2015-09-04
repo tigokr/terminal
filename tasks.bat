a752e508bf0773e29054366436f420093bd5e5b9
tigo <tigokr@gmail.com>
2015-09-04 17:39:01 +0300

    2.0.3

--- tasks.bat
+++ tasks.bat
@@ -10,10 +10,12 @@ schtasks /create /tn "Terminal 1m report" /f /sc MONTHLY /st 23:00 /ru terminal 
 schtasks /create /tn "Terminal alltime report" /f /sc MONTHLY /st 23:00 /ru terminal /tr "%TERM_PATH%lib\wget.exe http://localhost/reporter.php?get=1 -o NUL"
 
 schtasks /create /tn "Terminal update rests" /f /sc MINUTE /mo 15 /ru terminal /tr "%PHP_COMMAND% -c %PHP_INI% %WWW_PATH%\install\import_counts.php"
+schtasks /create /tn "Terminal update nomenclature" /f /sc DAILY /st 08:01 /ru terminal /tr "%PHP_COMMAND% -c %PHP_INI% %WWW_PATH%\install\import_items.php"
+schtasks /create /tn "Terminal update goods photos" /f /sc DAILY /st 08:15 /ru terminal /tr "%PHP_COMMAND% -c %PHP_INI% %WWW_PATH%\install\import_items_photos.php"
+schtasks /create /tn "Terminal update goods descriptions" /f /sc DAILY /st 08:15 /ru terminal /tr "%PHP_COMMAND% -c %PHP_INI% %WWW_PATH%\install\import_desc.php"
+
 schtasks /create /tn "Terminal update by car selection" /f /sc DAILY /st 01:01 /ru terminal /tr "%PHP_COMMAND% -c %PHP_INI% %WWW_PATH%\install\import.php"
-schtasks /create /tn "Terminal update nomenclature" /f /sc WEEKLY /d sun /st 03:01 /ru terminal /tr "%PHP_COMMAND% -c %PHP_INI% %WWW_PATH%\install\import_items.php"
-schtasks /create /tn "Terminal update goods photos" /f /sc WEEKLY /d sun /st 05:01 /ru terminal /tr "%PHP_COMMAND% -c %PHP_INI% %WWW_PATH%\install\import_items_photos.php"
-schtasks /create /tn "Terminal update goods descriptions" /f /sc WEEKLY /d sun /st 05:30 /ru terminal /tr "%PHP_COMMAND% -c %PHP_INI% %WWW_PATH%\install\import_desc.php"
+
 schtasks /create /tn "Terminal update www" /f /sc WEEKLY /d sun /st 00:01 /ru terminal /tr "%TERM_PATH%update.bat"
 
 schtasks /create /tn "Terminal kill metrointerface" /f /sc onlogon /ru terminal /delay 0000:10 /tr "%TERM_PATH%lib\metrokiller.exe /s"
